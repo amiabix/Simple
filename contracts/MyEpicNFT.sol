39e17dd4395e5450fcc45b3c6be9f2d08378194f
Ayush Bhadauria <72291803+ayushh007@users.noreply.github.com>
2022-05-24 17:55:20 +0530

    update

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -26,7 +26,7 @@ contract MyEpicNFT is ERC721URIStorage {
 
   function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
     uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
-    rand = rand % firstWords.length;
+    rand = rand % firstWords.length; 
     return firstWords[rand];
   }
 
